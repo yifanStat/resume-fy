03ebad8dbc0c36fcc61432dbf78ff38cab2f94bf
Yi Fan <yifan@Yis-MacBook-Pro.users.condointernet.net>
2020-01-04 12:55:35 -0800

    update footer

--- src/client/src/components/Footer.vue
+++ src/client/src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <v-footer fixed>
     <span>
-      &copy; 2019 Guang Yang
+      &copy; 2020 Yi Fan
       <!-- &nbsp;<v-icon color="primary">whatshot</v-icon> -->
     </span>
   </v-footer>
